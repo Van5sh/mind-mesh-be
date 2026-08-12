b13606ab710755bed8d170fe136a1b41c6b0eefb
Van5sh <vansh05dhir@gmail.com>
2026-08-12 22:42:04 +0530

    flowchar services complete

--- internal/services/flowchart.service.go
+++ internal/services/flowchart.service.go
@@ -1,6 +1,13 @@
 package services
 
-import "example/hello/internal/repository"
+import (
+	"context"
+	"example/hello/internal/database"
+	"example/hello/internal/repository"
+	"example/hello/internal/validators"
+
+	"github.com/jackc/pgx/v5/pgtype"
+)
 
 type FlowchartService struct {
 	repo *repository.FlowchartRepository
@@ -8,4 +15,72 @@ type FlowchartService struct {
 
 func NewFlowchartService(repo *repository.FlowchartRepository) *FlowchartService {
 	return &FlowchartService{repo: repo}
+}
+
+func (s *FlowchartService) GetFlowchartByID(ctx context.Context, id pgtype.UUID) (database.Flowchart, error) {
+	err := validators.ValidateUUID("flowchart_id", id)
+	if err != nil {
+		return database.Flowchart{}, err
+	}
+	flowchart, err := s.repo.GetFlowchartByID(ctx, id)
+	if err != nil {
+		return database.Flowchart{}, err
+	}
+	return flowchart, nil
+}
+
+func (s *FlowchartService) CreateFlowchart(
+	ctx context.Context,
+	params database.CreateFlowchartParams,
+) (database.Flowchart, error) {
+
+	err := validators.ValidateUUID("project_id", params.ProjectID)
+	if err != nil {
+		return database.Flowchart{}, err
+	}
+
+	err = validators.ValidateFlowchartName(params.Name)
+	if err != nil {
+		return database.Flowchart{}, err
+	}
+
+	err = validators.ValidateFlowchartData(params.Data)
+	if err != nil {
+		return database.Flowchart{}, err
+	}
+
+	flowchart, err := s.repo.CreateFlowchart(ctx, params)
+	if err != nil {
+		return database.Flowchart{}, err
+	}
+
+	return flowchart, nil
+}
+
+func (s *FlowchartService) UpdateFlowchart(ctx context.Context, params database.UpdateFlowchartParams) (database.Flowchart, error) {
+	err := validators.ValidateUUID("flowchart_id", params.ID)
+	if err != nil {
+		return database.Flowchart{}, err
+	}
+	err = validators.ValidateFlowchartName(params.Name)
+	if err != nil {
+		return database.Flowchart{}, err
+	}
+	flowchart, err := s.repo.UpdateFlowchart(ctx, params)
+	if err != nil {
+		return database.Flowchart{}, err
+	}
+	return flowchart, nil
+}
+
+func (s *FlowchartService) DeleteFlowchart(ctx context.Context, id pgtype.UUID) error {
+	err := validators.ValidateUUID("flowchart_id", id)
+	if err != nil {
+		return err
+	}
+	err = s.repo.DeleteFlowchart(ctx, id)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- internal/validators/flowchart.validator.go
+++ internal/validators/flowchart.validator.go
@@ -1,10 +1,27 @@
 package validators
 
-import "example/hello/internal/apperrors"
+import (
+	"encoding/json"
+
+	"example/hello/internal/apperrors"
+)
 
 func ValidateFlowchartName(name string) error {
 	if err := ValidateRequiredString("flowchart name", name); err != nil {
 		return apperrors.Validation("Flowchart name is required")
 	}
+
 	return ValidateMaxLength("flowchart name", name, 100)
+}
+
+func ValidateFlowchartData(data []byte) error {
+	if len(data) == 0 {
+		return apperrors.Validation("Flowchart data is required")
+	}
+
+	if !json.Valid(data) {
+		return apperrors.Validation("Invalid flowchart JSON")
+	}
+
+	return nil
 }
